3887488024cd44471f81a804d545123d34664f69
Jesus Jimenez Cordero <jesus_heaven117@outlook.com>
2019-04-28 10:51:50 -0500

    Capitulo 83 de Ionic - Fernando Herrara. Continuando...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SubirPage } from './pages/subir/subir.page';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
+  declarations: [
+    AppComponent,
+    SubirPage
+  ],
+  entryComponents: [
+    SubirPage
+  ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
